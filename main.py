b384ba9eb7481c7e25f34442f2a627ad3a0a3021
limiyama <lmizuyama@gmail.com>
2025-06-08 14:47:37 -0300

    kjhkk

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def extract_bgcs_from_antismash(gbk_dir):
     for gbk_file in glob.glob(os.path.join(gbk_dir, "*.gbk*")):
         for record in SeqIO.parse(gbk_file, "genbank"):
             organism = record.annotations.get("organism", "Unknown")
+            filename = organism + ".txt"
             features = record.features
 
-            for feature in features:
-                # descriptions - nomes dos domínios e tailoring enzymes
-                desc = feature.qualifiers.get("description", "N/A")[0].lower()
-                with open("path_to_and_name_of_file","mode") as variable_name:
-                    variable_name.write(desc)
+            with open(filename, "w", encoding="utf-8") as f:
+                    f.write(f"ORGANISM: {organism} \n")
+                    
+                    for feature in features:
+                        # descriptions - nomes dos domínios e tailoring enzymes
+                        desc = feature.qualifiers.get("description", "N/A")[0].lower()
+                        f.write(desc + "\n")
                 
 
 #gbk_file = 'mibig_gbk_4.0/BGC0001000.gbk'  # Replace with your .gbk file
